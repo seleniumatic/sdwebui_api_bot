b630b5839416117f4ebf946e5d6cb3e868baf2ed
seleniumatic <127901273+seleniumatic@users.noreply.github.com>
2023-10-02 00:13:53 -0700

    fixed wiremock test

--- src/test/java/com/seleniumatic/sd/SdApiHelperTest.java
+++ src/test/java/com/seleniumatic/sd/SdApiHelperTest.java
@@ -6,36 +6,33 @@ import java.net.URISyntaxException;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import com.seleniumatic.sd.common.SdApiClient;
 
 public class SdApiHelperTest {
 
-    private WireMockServer wireMockServer;
     private SdApiClient SdApiHelper;
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8089);
 
     @Before
     public void setup() {
-        WireMockConfiguration wireMockConfig = WireMockConfiguration.options().port(8080);
-        wireMockServer = new WireMockServer(wireMockConfig);
-        wireMockServer.start();
-
-       SdApiHelper = new SdApiClient("http://localhost:8080/data", "{\"prompt\": \"cat\"}");
-    }
-
-    @After
-    public void tearDown() {
-        wireMockServer.shutdown();;
+        SdApiHelper = new SdApiClient("http://localhost:8089/data", "{\"prompt\": \"cat\"}");
     }
 
     @Test
     public void testHttpGetRequest() throws URISyntaxException {
         String expectedResult = "{\"message\":\"Get request content\"}";
-        
+
         stubFor(get(urlEqualTo("/data"))
             .willReturn(aResponse()
             .withStatus(200)
@@ -51,18 +48,16 @@ public class SdApiHelperTest {
     public void testHttpPostRequest() throws URISyntaxException {
         String expectedResult = "{\"message\":\"Post request content\"}";
 
-        stubFor(get(urlEqualTo("/data"))
+        System.out.println("test2");
+
+        stubFor(post(urlEqualTo("/data"))
             .willReturn(aResponse()
             .withStatus(200)
             .withHeader("Content-Type", "application/json")
             .withBody(expectedResult)));
 
-        String actualResult = SdApiHelper.httpGetRequest();
+        String actualResult = SdApiHelper.httpPostRequest();
        
         assertEquals(expectedResult, actualResult);
-    }
-
-
-
-    
+    }   
 }
